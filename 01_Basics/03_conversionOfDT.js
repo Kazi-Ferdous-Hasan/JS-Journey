40ba3ebd96e510419e0e14b35e9aec53c4856074
Kazi Ferdous Hasan <118041179+Kazi-Ferdous-Hasan@users.noreply.github.com>
2023-09-20 07:36:57 +0000

    Operations

--- 01_Basics/03_conversionOfDT.js
+++ 01_Basics/03_conversionOfDT.js
@@ -16,8 +16,8 @@ let isLoggedIn = "Hasan"
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
 
-console.log(typeof booleanIsLoggedIn);
-console.log(booleanIsLoggedIn);
+//console.log(typeof booleanIsLoggedIn);
+//console.log(booleanIsLoggedIn);
 
 // if you convert,
 // 1 => true; 0 =>false
@@ -26,5 +26,46 @@ console.log(booleanIsLoggedIn);
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(typeof stringNumber);
-console.log(stringNumber);
+//console.log(typeof stringNumber);
+//console.log(stringNumber);
+
+// ---Operations---
+
+let value = 3
+let negValue = -value
+//console.log(negValue)
+
+// Basic Arithmatic Operations
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2**3);
+// console.log(2%2);
+
+let str1 = "Hello"
+let str2 = " Hasan"
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2); // output: 12
+// console.log(1 + "2"); // output: 12
+// console.log("1" + 2 + 2); // will be treated as string. Output: 122
+// console.log(1 + 2 + "2"); // will be treated as numbers. First add 1+2 then string so output: 32
+
+// console.log(3+4*5%3); Not readable 
+
+//Not preferable to use
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+num1 = num2 = num3 =  2+2 
+
+let gameCounter = 100 
+++gameCounter;
+console.log(gameCounter);
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
+
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
