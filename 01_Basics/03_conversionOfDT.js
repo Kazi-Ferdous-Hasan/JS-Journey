3b4c1a16880954d17c7aaad56f46ee16e7ad2bb8
Kazi Ferdous Hasan <118041179+Kazi-Ferdous-Hasan@users.noreply.github.com>
2023-09-20 07:06:03 +0000

    conversion of data types

---
+++ 01_Basics/03_conversionOfDT.js
@@ -0,0 +1,30 @@
+let score = "Hasan"
+
+//console.log(typeof score);
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+
+//console.log(valueInNumber);
+
+// If you convert,
+// "33" => 33
+// "33ab" => NaN (Not a Number)
+// true => 1; false => 0
+
+let isLoggedIn = "Hasan"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+console.log(typeof booleanIsLoggedIn);
+console.log(booleanIsLoggedIn);
+
+// if you convert,
+// 1 => true; 0 =>false
+// "" => false
+// "hasan" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber);
+console.log(stringNumber);
