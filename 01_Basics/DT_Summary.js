7c6405d028419b43cc3c7d7d8858ad06fd520904
Kazi Ferdous Hasan <118041179+Kazi-Ferdous-Hasan@users.noreply.github.com>
2023-09-22 14:41:35 +0000

    update

--- 01_Basics/DT_Summary.js
+++ 01_Basics/DT_Summary.js
@@ -68,5 +68,3 @@ console.log(typeof anotherId);
 // Stack (Primitive) 
 // Heap (Non-Primitive)
 // reference: https://www.youtube.com/watch?v=7gwc-1czolw&list=PLu71SKxNbfoBuX3f4EOACle2y-tRC5Q37&index=11
-
-
